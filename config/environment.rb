37ef6e4df1320c7a7eb9362695cf188001259688
oladipeter <oladipeter@gmail.com>
2011-08-10 11:46:19 +0200

    mod environments rb

--- config/environment.rb
+++ config/environment.rb
@@ -4,6 +4,5 @@ require File.expand_path('../application', __FILE__)
 # default environment - production
 # ENV['RAILS_ENV'] = 'production'
 
-
 # Initialize the rails application
 Mikrokamnew::Application.initialize!
